be6c17c940efe504a6f60a8f5a6f5bcc099ae6df
Whatruska <vitdub@inbox.ru>
2019-11-06 20:44:35 +0300

    login form added

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -1,5 +1,4 @@
 package GUI;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,8 +10,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Login/LoginForm.fxml"));
+        root.getStylesheets().add(getClass().getResource("css/style.css").toExternalForm());
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
